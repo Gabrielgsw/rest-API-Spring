85b0e92e7df6bd42990b57013c1a124eca1ae520
Gabrielgsw <ggermanow279@gmail.com>
2024-10-04 21:18:26 -0300

    initial commit

---
+++ src/test/java/br/com/restAPI/RestApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
